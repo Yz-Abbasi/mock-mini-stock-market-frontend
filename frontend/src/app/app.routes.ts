fd6a741ec47a14657405ba6e0d4b8f1666d8132b
Yazdan abbasi <yabasi891@gmail.com>
2024-09-10 00:18:27 +0330

    Fixed data validation. Added redirection to home page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/forms/login/login.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
+    {path:'Home', component:AppComponent},
     {path:'Login', component:LoginComponent},
+    {path:' ', redirectTo:'/Home', pathMatch:'full'},
 ];
--- frontend/src/app/components/forms/login/login.component.ts
+++ frontend/src/app/components/forms/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { User } from '../../../models/user';
 import { FormsModule, FormControl, FormGroup } from '@angular/forms';
-import { Console } from 'console';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +12,11 @@ import { Console } from 'console';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  router: Router;
+  constructor(private http : HttpClient, router : Router)
+  {
+    this.router = router;
+  }
   user : User = new User
   (
     '',
@@ -27,5 +32,6 @@ export class LoginComponent {
     console.log(formValues);
     console.log('on submit function :' + this.user.username + this.user.password);
     this.submitted = true;
+    this.router.navigate(['']);
   }
 }
